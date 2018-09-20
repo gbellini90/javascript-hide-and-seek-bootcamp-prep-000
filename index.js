4f6a130d7458d0d8927d004cd527ed45d531c5d2
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-20 17:38:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  const firstSelector=document.querySelector(selector)
+  return firstSelector
+}
 
+function nestedTarget(){
+  const nest=document.getElementById('nested').querySelector('.target')
+  return nest
+}
+
+function increaseRankBy(n){
+  const rank=document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i=0;i<rank.length;i++){
+    rank[i].innerHTML=(parseInt(rank[i].innerHTML)+n)
+  }
+  return rank
+}
+
+function deepestChild(){
+  const child=document.querySelector('#grand-node div div div div')
+return child
+  }
